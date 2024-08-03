bbd42134352e1138ff064427cd80a80329d6c0d2
TrueMB <>
2024-08-03 13:53:33 +0200

    Fix: Add Main Class for reference

---
+++ src/main/java/me/truemb/rentitworlds/main/RIW.java
@@ -0,0 +1,13 @@
+package me.truemb.rentitworlds.main;
+
+import me.truemb.rentitworlds.api.RiwAPI;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RIW extends JavaPlugin {
+
+	//API
+	public static RiwAPI getAPI() {
+		return null;
+	}
+
+}
